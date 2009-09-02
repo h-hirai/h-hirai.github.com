f4a8c1e426e33aea131a1fe1b83912ea75bea6c1
HIRAI, Hajime <hirai.hajime@gmail.com>
2009-09-02 17:42:00 +0900

    fix typo
    	modified:   js_svg/led_matrix.js

--- js_svg/led_matrix.js
+++ js_svg/led_matrix.js
@@ -1,13 +1,13 @@
 function init_matrix() {
-  var led_r = 5;
+  var led_r = 10;
   var nx = 8;
   var ny = 8;
-  var margin = 5;
+  var margin = 1;
 
   var svg = document.getElementById('SVG');
 
   svg.setAttribute("width", margin * 2 + (led_r * 2) * nx);
-  svg.setAttribute("heigth", margin * 2 + (led_r * 2) * ny);
+  svg.setAttribute("height", margin * 2 + (led_r * 2) * ny);
 
   for (var y = 0; y < ny; y++) {
     for (var x = 0; x < nx; x++) {
