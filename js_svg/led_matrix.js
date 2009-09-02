a4899b84bd0916e70d1fc79e91a8c3415c6e214d
HIRAI, Hajime <hirai.hajime@gmail.com>
2009-09-02 17:28:58 +0900

    add margin and parametrize
    	modified:   js_svg/led_matrix.js
    	modified:   js_svg/led_matrix.xml

--- js_svg/led_matrix.js
+++ js_svg/led_matrix.js
@@ -1,16 +1,24 @@
 function init_matrix() {
-    var svg = document.getElementById('SVG');
-    for (var y = 0; y < 8; y++) {
-        for (var x = 0; x < 8; x++) {
-            var c = document.createElementNS("http://www.w3.org/2000/svg",
-                "circle");
-            c.setAttribute("id", "x" + x + "y" + y);
-            c.setAttribute("cx", x * 10 + 5);
-            c.setAttribute("cy", y * 10 + 5);
-            c.setAttribute("r", "5");
-            c.setAttribute("stroke", "red");
-            c.setAttribute("fill", "white");
-            svg.appendChild(c);
-        }
+  var led_r = 5;
+  var nx = 8;
+  var ny = 8;
+  var margin = 5;
+
+  var svg = document.getElementById('SVG');
+
+  svg.setAttribute("width", margin * 2 + (led_r * 2) * nx);
+  svg.setAttribute("heigth", margin * 2 + (led_r * 2) * ny);
+
+  for (var y = 0; y < ny; y++) {
+    for (var x = 0; x < nx; x++) {
+      var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+      c.setAttribute("id", "x" + x + "y" + y);
+      c.setAttribute("cx", x * (led_r * 2) + led_r + margin);
+      c.setAttribute("cy", y * (led_r * 2) + led_r + margin);
+      c.setAttribute("r", led_r);
+      c.setAttribute("stroke", "red");
+      c.setAttribute("fill", "white");
+      svg.appendChild(c);
     }
+  }
 }
