a9c5b32402dd435046ba98b1630159053a0e3ce9
HIRAI, Hajime <hirai.hajime@gmail.com>
2009-09-06 18:02:07 +0900

    make animation
    	modified:   js_svg/led_matrix.data
    	modified:   js_svg/led_matrix.js

--- js_svg/led_matrix.js
+++ js_svg/led_matrix.js
@@ -2,44 +2,57 @@ var NumX = 8;
 var NumY = 8;
 
 function switch_led(x, y, state) {
-  var c = document.getElementById("x" + x + "y" + y);
-  c.setAttribute("class", state);
+    var c = document.getElementById("x" + x + "y" + y);
+    c.setAttribute("class", state);
 }
 
-function update_matrix(ary) {
-  for (var y = 0; y < NumY; y++) {
-    for (var x = 0; x < NumX; x++){
-      switch_led(x, y, ary[y][x] == "1" ? "on" : "off");
+function update_matrix(sig) {
+    for (var y = 0; y < NumY; y++) {
+        if (sig[y + 8] == "1") {
+            for (var x = 0; x < NumX; x++) {
+                switch_led(x, y, sig[x] == "1" ? "on" : "off");
+            }
+        } else {
+            for (var x = 0; x < NumX; x++) {
+                switch_led(x, y, "off");
+            }
+        }
     }
-  }
 }
 
-function load_data_and_update_matrix() {
-  var req = new XMLHttpRequest();
-  req.open('GET', './led_matrix.data', false);
-  req.send(null);
-  update_matrix(req.responseText.split("\n"));
+function load_data() {
+    var req = new XMLHttpRequest();
+    req.open('GET', './led_matrix.data', false);
+    req.send(null);
+
+    var sig_seq = req.responseText.split("\n");
+    var cnt_seq = 0;
+    setInterval(function() {
+                    update_matrix(sig_seq[cnt_seq]);
+                    cnt_seq = cnt_seq >= sig_seq.length - 1 ? 0 : cnt_seq + 1;
+                }, 100);
 }
 
 function init_matrix() {
-  var LedR = 10;
-  var Margin = 1;
+    var LedR = 10;
+    var Margin = 1;
 
-  var svg = document.getElementById("SVG");
-  svg.onclick = load_data_and_update_matrix;
+    var svg = document.getElementById("SVG");
+    svg.onclick = load_data;
 
-  svg.setAttribute("width", Margin * 2 + (LedR * 2) * NumX);
-  svg.setAttribute("height", Margin * 2 + (LedR * 2) * NumY);
+    svg.setAttribute("width", Margin * 2 + (LedR * 2) * NumX);
+    svg.setAttribute("height", Margin * 2 + (LedR * 2) * NumY);
 
-  for (var y = 0; y < NumY; y++) {
-    for (var x = 0; x < NumX; x++) {
-      var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-      c.setAttribute("id", "x" + x + "y" + y);
-      c.setAttribute("cx", x * (LedR * 2) + LedR + Margin);
-      c.setAttribute("cy", y * (LedR * 2) + LedR + Margin);
-      c.setAttribute("r", LedR);
-      c.setAttribute("class", "off");
-      svg.appendChild(c);
+    for (var y = 0; y < NumY; y++) {
+        for (var x = 0; x < NumX; x++) {
+            var c = document.createElementNS("http://www.w3.org/2000/svg",
+                                             "circle");
+            c.setAttribute("id", "x" + x + "y" + y);
+            c.setAttribute("cx", x * (LedR * 2) + LedR + Margin);
+            c.setAttribute("cy", y * (LedR * 2) + LedR + Margin);
+            c.setAttribute("r", LedR);
+            c.setAttribute("class", "off");
+            svg.appendChild(c);
+        }
     }
-  }
 }
